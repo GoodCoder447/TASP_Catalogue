68d3e614f3484d774818e9c397ea92b1c02db0ec
houghj2 <houghj2@rpi.edu>
2016-12-05 14:02:00 -0500

    more changes to accommodate photo and checkout

--- Backend/public/js/checkout.js
+++ Backend/public/js/checkout.js
@@ -12,4 +12,41 @@ function getParameter(name, url) {
 
 var foo = getParameter('id');
 
+var socket=io();
+
+
+angular.module('app',[]).controller('profile', function($scope, $http) {
+    $scope.user = '';
+	var getitem = function() {
+		socket.emit('getItem', foo);
+	}
+    getitem();
+
+
+    
+socket.on('receiveItem',function(list){
+    console.log(list);
+	$scope.$apply(function() {
+		$scope.items = list;
+        if (list.table == 'items') {
+            $scope.title = 'Thank You !';
+            $scope.message = '';
+            var item = {
+		      table: 'items',
+              newtable: 'pickup',
+              paid: false,
+		      name: list
+	        }
+	        socket.emit('editItem',item);
+        }else {
+            $scope.title = 'Sorry !';
+            $scope.message = '';
+        }
+	});
+ 
+})
+
+
+ 
+});
 
